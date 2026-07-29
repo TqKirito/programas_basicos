9bc5ef18ac251383547ecce8776e9f014db09c07
Ivan Alonso Reyes <26150626@aguascalientes.tecnm.mx>
2026-07-29 16:53:04 -0600

    Agregar Actividad 4

--- Actividad_4/divisibles_3_y_5.py
+++ Actividad_4/divisibles_3_y_5.py
@@ -3,8 +3,7 @@
 print("Números divisibles por 3 y 5 (1-100):")
 
 for i in range(1, 101):
-
     if i % 3 == 0 and i % 5 == 0:
         print(i, end=" ")
 
-print()  
+print()  # salto de línea final
